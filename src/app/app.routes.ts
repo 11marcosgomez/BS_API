c4ca31dadc897d89c4e4c72beebb2bf7532dbba8
MarcosG <Marcos@gmail.com>
2024-05-10 20:39:13 +0200

    Cambios viernes 10 de Mayo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { InicioSesionComponent } from './componentes/inicio-sesion/inicio-sesion.component';
+import { BrawlersComponent } from './componentes/brawlers/brawlers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "",
+    redirectTo:"inicio",
+    pathMatch: 'full'
+},{
+    path :"inicio",
+    component:InicioComponent
+},{
+    path:"registro",
+    component:RegistroComponent
+},{
+    path:"inicio sesion",
+    component:InicioSesionComponent
+},{
+    path:"brawler",
+    component:BrawlersComponent
+}
+
+
+]
+
---
+++ src/app/componentes/brawlers/brawlers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brawlers',
+  standalone: true,
+  imports: [],
+  templateUrl: './brawlers.component.html',
+  styleUrl: './brawlers.component.css'
+})
+export class BrawlersComponent {
+
+}
